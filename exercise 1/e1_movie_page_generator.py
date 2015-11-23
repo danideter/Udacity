effa45b5045da4b6c035c5ab9c8b25756500ec44
danideter <danideter@gmail.com>
2015-11-23 00:31:25 -0800

    Update: Clean Comments
    Added spaces between # and comments

--- exercise 1/e1_movie_page_generator.py
+++ exercise 1/e1_movie_page_generator.py
@@ -16,9 +16,9 @@ Returns:
 
 import fresh_tomatoes_mod
 
-#Create movie class to add movie properties as html in fresh_tomatos_mod
-#Pylint suggests I used a dictionary instead? But the instructions say to make a
-#movie class, so I'm going with that.
+# Create movie class to add movie properties as html in fresh_tomatos_mod
+# Pylint suggests I used a dictionary instead? But the instructions say to make a
+# movie class, so I'm going with that.
 class Movie(object):
     """ This class creates a movie data atructure to be passed on to
     fres_tomatos_mod.
@@ -37,7 +37,7 @@ class Movie(object):
         self.trailer_youtube_url = trailer
         self.poster_image_url = poster
 
-#Create three instances to contain the best trology ever
+# Create three instances to contain the best trology ever
 ALIEN = Movie("Alien", "https://www.youtube.com/watch?v=LjLamj-b0I8",
               "http://40.media.tumblr.com/bc63f2e52e1afc5b33d1c80e993e7fdf/tumblr_mnulojVUcd1qzcgluo1_1280.jpg")
 ALIENS = Movie("Aliens", "https://www.youtube.com/watch?v=XKSQmYUaIyE",
@@ -45,5 +45,5 @@ ALIENS = Movie("Aliens", "https://www.youtube.com/watch?v=XKSQmYUaIyE",
 ALIEN3 = Movie("Aliens3", "https://www.youtube.com/watch?v=UVb4Y49L7_U",
                "http://www.impawards.com/1992/posters/alien_three_ver2.jpg")
 
-#Run a modded version of fresh_tomatos
+# Run a modded version of fresh_tomatos
 fresh_tomatoes_mod.open_movies_page([ALIEN, ALIENS, ALIEN3])
